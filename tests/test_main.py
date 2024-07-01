f0d56a09d283c80731c3d60d94dd838317b961c3
yoshin04 <taigaby0411@gmail.com>
2024-07-02 00:37:00 +0900

    wip

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,7 +10,7 @@ from api.main import app
 ASYNC_DB_URL = "sqlite+aiosqlite:///:memory:"
 
 @pytest.fixture
-async def async_client() -> AsyncClient:
+async def async_client():
     # Async用のengineとsessionを作成
     async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
     async_session = sessionmaker(
